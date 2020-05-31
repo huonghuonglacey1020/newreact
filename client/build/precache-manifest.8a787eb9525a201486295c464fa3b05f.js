0dd390e8d0bcbb515979c659fa0f9130d9965021
huongnguyen <huonghuonglacey1020@gmail.com>
2020-05-30 21:41:25 -0700

    Refreshes git without submodule

---
+++ client/build/precache-manifest.8a787eb9525a201486295c464fa3b05f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6af612ec5397e120fb9c",
+    "url": "/static/css/main.acee946c.chunk.css"
+  },
+  {
+    "revision": "6af612ec5397e120fb9c",
+    "url": "/static/js/main.81279ddd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "59a31224cef2e91d7549",
+    "url": "/static/js/2.46518fec.chunk.js"
+  },
+  {
+    "revision": "9f2b4dd6e5a20e7c9ee5e6927e8de17e",
+    "url": "/static/media/bookshelf.9f2b4dd6.jpg"
+  },
+  {
+    "revision": "85e4bc328080f269a4d696f202a841c8",
+    "url": "/index.html"
+  }
+];
---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron jumbotron-fluid d-flex align-items-center">
+            <div className="container text-center">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Jumbotron;
